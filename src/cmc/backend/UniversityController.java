b9dc8d020e1fc87fc42a72617f1585b86a79f4f9
Roman Lefler <rlefler001@csbsju.edu>
2025-04-02 08:45:26 -0500

    Move database interaction from Uni.Controller to DatabaseController

--- src/cmc/backend/UniversityController.java
+++ src/cmc/backend/UniversityController.java
@@ -14,20 +14,17 @@ import cmc.backend.entities.University;
 import dblibrary.project.csci230.UniversityDBLibrary;
 
 /**
- * 
+ * University controller
+ * @author Timmy Flynn, Roman Lefler
+ * @version Apr 2, 2025
  */
 public class UniversityController {
 
 	
 	private DatabaseController db;
 	
-	private UniversityDBLibrary database;
-
-	private static final String SHOULDNT_HAPPEN = "If you're seeing this DatabaseController has a bug.";
-	
 	public UniversityController() {
 		this.db = new DatabaseController();
-		this.database = new UniversityDBLibrary("dei", "Csci230$");
 	}
 	
 	/**
@@ -40,22 +37,8 @@ public class UniversityController {
 	 * @version Mar 13, 2025
 	 */
 	public boolean addNewUniversity(University u) {
-		int result = database.university_addUniversity(
-				u.getName(), u.getState(), u.getLocation(), u.getControl(),
-				u.getNumStudents(), u.getPercentFemale(), u.getSatVerbal(),
-				u.getSatMath(), u.getExpenses(), u.getPercentFinancialAid(),
-				u.getNumApplicants(), u.getPercentAdmitted(),
-				u.getPercentEnrolled(), u.getScaleAcademics(),
-				u.getScaleSocial(), u.getScaleQualityOfLife());
 		
-		if(result != 1) return false;
-		
-		String uniName = u.getName();
-		for(String e : u.getEmphases()) {
-			if(!db.addEmphasis(uniName, e)) throw new IllegalStateException(SHOULDNT_HAPPEN);
-		}
-		
-		return true;
+		return db.addNewUniversity(u);
 	}
 	
 	/**
@@ -70,22 +53,7 @@ public class UniversityController {
 	 */
 	public boolean removeUniversity(University u) {
 		
-		if(u == null) throw new IllegalArgumentException("u cannot be null.");
-		
-		String uniName = u.getName();
-		// Since u's emphasis list can be out of sync with the database's
-		// emphases, we must rely on database's reported emphases
-		Map<String, List<String>> emphasesDict = getUniversitiesEmphases();
-		List<String> emphases = emphasesDict.get(uniName);
-		if(emphases != null) {
-			for(String k : u.getEmphases()) {
-				
-				if(!db.removeEmphasis(uniName, k)) throw new IllegalStateException(SHOULDNT_HAPPEN);
-			}
-		}
-		
-		int result = database.university_deleteUniversity(u.getName());
-		return result > 0;
+		return db.removeUniversity(u);
 	}
 	
 	/**
@@ -100,42 +68,7 @@ public class UniversityController {
 	 */
 	public boolean editUniversity(University u) {
 		
-		// # Emphases:
-		// Anything that is present in the database but not u
-		// was removed.
-		// Anything that is present in u but not in the database
-		// was added.
-		String uniName = u.getName();
-		University old = University.find(getAllSchools(), uniName);
-		List<String> oldE = old.getEmphases();
-		List<String> newE = u.getEmphases();
-		// Create a total set of all emphases
-		Set<String> both = new HashSet<>(oldE);
-		both.addAll(newE);
-		// Iterate through the total set of all emphases
-		for(String em : both) {
-			if(oldE.contains(em)) {
-				// In old but not new; Newly removed
-				if(!newE.contains(em)) {
-					if(!db.removeEmphasis(uniName, em)) throw new IllegalStateException(SHOULDNT_HAPPEN);
-				}
-			}
-			// Not in old (and therefore must be in new); Newly added
-			else {
-				if(!db.addEmphasis(uniName, em)) throw new IllegalStateException(SHOULDNT_HAPPEN);
-			}
-		}
-		
-		// Here's the easy part
-		int result = database.university_editUniversity(
-				u.getName(), u.getState(), u.getLocation(), u.getControl(),
-				u.getNumStudents(), u.getPercentFemale(), u.getSatVerbal(),
-				u.getSatMath(), u.getExpenses(), u.getPercentFinancialAid(),
-				u.getNumApplicants(), u.getPercentAdmitted(),
-				u.getPercentEnrolled(), u.getScaleAcademics(),
-				u.getScaleSocial(), u.getScaleQualityOfLife());
-		
-		return result == 1;
+		return db.editUniversity(u);
 	}
 	
 	/**
@@ -145,36 +78,8 @@ public class UniversityController {
 	 * @version Mar 13, 2025
 	 */
 	public List<University> getAllSchools() {
-		String[][] dbUniversityList = this.database.university_getUniversities();
-
-		Map<String, List<String>> emphases = getUniversitiesEmphases();
-		ArrayList<University> result = new ArrayList<>();
-		for (String[] k : dbUniversityList) {
-			University u = new University(k[0]);
-			u.setState(k[1]);
-			u.setLocation(k[2]);
-			u.setControl(k[3]);
-			u.setNumStudents(Integer.parseInt(k[4]));
-			u.setPercentFemale(Double.parseDouble(k[5]));
-			u.setSatVerbal(Double.parseDouble(k[6]));
-			u.setSatMath(Double.parseDouble(k[7]));
-			u.setExpenses(Double.parseDouble(k[8]));
-			u.setPercentFinancialAid(Double.parseDouble(k[9]));
-			u.setNumApplicants(Integer.parseInt(k[10]));
-			u.setPercentAdmitted(Double.parseDouble(k[11]));
-			u.setPercentEnrolled(Double.parseDouble(k[12]));
-			u.setScaleAcademics(Integer.parseInt(k[13]));
-			u.setScaleSocial(Integer.parseInt(k[14]));
-			u.setScaleQualityOfLife(Integer.parseInt(k[15]));
-			List<String> schoolEmphases = emphases.get(k[0]);
-			if(schoolEmphases != null) {
-				for(String e : schoolEmphases) u.addEmphasis(e);
-			}
-			
-			result.add(u);
-		}
-
-		return result;
+		
+		return db.getAllSchools();
 	}
 	
 	/**
@@ -188,18 +93,8 @@ public class UniversityController {
 	 * @version Mar 14, 2025
 	 */
 	public Map<String, List<String>> getUniversitiesEmphases() {
-		String[][] emphases = database.university_getNamesWithEmphases();
-		Map<String, List<String>> dict = new HashMap<String, List<String>>();
-		for(String[] kv : emphases) {
-			
-			List<String> list = dict.get(kv[0]);
-			if(list == null) {
-				list = new ArrayList<>(3);
-				dict.put(kv[0], list);
-			}
-			list.add(kv[1]);
-		}
-		return dict;
+		
+		return db.getUniversitiesEmphases();
 	}
 		
 }
