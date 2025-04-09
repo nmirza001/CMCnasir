06f1176e996613d006a21b5a926be62d870a11b5
ALOPEZ003 <alopez003@vplinacad18.ad.csbsju.edu>
2025-04-09 12:02:53 -0500

    ImplementedUser entity

---
+++ src/cmc/backend/entities/User.java
@@ -0,0 +1,41 @@
+package cmc.backend.entities;
+import java.util.ArrayList;
+import java.util.List;
+
+
+//public class User extends Account {
+	//private List <SavedSchool> savedSchools;
+	
+	//public User(String Username, String Password, String firstName, String lastName) {
+		//super(username,password,firstName, lastName);
+		//this.savedSchools = new ArrayList<>();
+	//}
+	//@Override
+	//public boolean isAdmin() {
+		//return false;
+	//}
+	/**
+	 * @param savedSchool the school to save
+	 * @return true if added successfully, false otherwise
+	 */
+	//public boolean saveSchool(SavedSchool savedSchool) {
+		//if (!savedSchools.contains(savedSchool)) {
+			//savedSchools,add(savedSchool);
+			//return true;
+		//}
+		//return false;
+	//}
+	/**
+	 * @param savedSchool the school to remove
+	 * @return true if removed, false otherwise
+	 */
+	//public boolean removeSavedSchool(SavedSchool savedSchool) {
+		//return savedSchools.remove(savedSchool);
+	//}
+	//public List<SavedSchool> getSavedSchools(){
+		//return this.savedSchools;
+	//}
+	//public List<SavedSchool> search(String searchCriteria){
+		//return new ArrayList<>();
+	//}
+//}
