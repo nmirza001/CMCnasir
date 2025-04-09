729a1e909d822cc54b8ea993de3ee24669005d69
ALOPEZ003 <alopez003@vplinacad05.ad.csbsju.edu>
2025-04-09 12:55:41 -0500

    Upadted Implementation of User entity

--- src/cmc/backend/entities/User.java
+++ src/cmc/backend/entities/User.java
@@ -1,12 +1,11 @@
 package cmc.backend.entities;
 import java.util.ArrayList;
 import java.util.List;
-
-
 //public class User extends Account {
 	//private List <SavedSchool> savedSchools;
 	
-	//public User(String Username, String Password, String firstName, String lastName) {
+	//public User(String username, String password, String firstName, String lastName) {
+
 		//super(username,password,firstName, lastName);
 		//this.savedSchools = new ArrayList<>();
 	//}
@@ -18,9 +17,10 @@ import java.util.List;
 	 * @param savedSchool the school to save
 	 * @return true if added successfully, false otherwise
 	 */
-	//public boolean saveSchool(SavedSchool savedSchool) {
-		//if (!savedSchools.contains(savedSchool)) {
-			//savedSchools,add(savedSchool);
+	//public boolean saveSchool(University university) {
+		//SavedSchool newEntry = new SavedSchool(university);
+		//if (!savedSchools.contains(newEntry)) {
+			//savedSchools.add(newEntry);
 			//return true;
 		//}
 		//return false;
@@ -29,13 +29,19 @@ import java.util.List;
 	 * @param savedSchool the school to remove
 	 * @return true if removed, false otherwise
 	 */
-	//public boolean removeSavedSchool(SavedSchool savedSchool) {
-		//return savedSchools.remove(savedSchool);
+	//public boolean removeSavedSchool(University university) {
+		//for (SavedSchool saved : savedSchools) {
+			//if (saved.getUniversity().equals(university)) {
+				//savedSchools.remove(saved);
+				//return true;
+			//}
+		//}
+		//return false;
 	//}
 	//public List<SavedSchool> getSavedSchools(){
 		//return this.savedSchools;
 	//}
 	//public List<SavedSchool> search(String searchCriteria){
-		//return new ArrayList<>();
+		//return new ArrayList<>(savedSchhols);
 	//}
 //}
