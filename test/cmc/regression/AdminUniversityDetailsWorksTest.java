5e4e958ab21c1b3f9a394650ae91d701d78303c7
RMASAANA001 <rmasaana001@vplinacad15.ad.csbsju.edu>
2025-04-15 22:22:09 -0500

    minor edit to see if getEditLoc passes CI test #3

--- test/cmc/regression/AdminUniversityDetailsWorksTest.java
+++ test/cmc/regression/AdminUniversityDetailsWorksTest.java
@@ -76,9 +76,9 @@ public class AdminUniversityDetailsWorksTest {
 		    String input = "2\n" //University Menu
 						+ "4\n" //Edit University
 						+ "1\n" //Selecting the first only uni
-						+ "URBAN\n" //Name (don't change)
-						+ "\n" //New State
-						+ "\n\n\n\n\n\n\n\n\n\n\n\n\n" //all other fields ignored
+						+ "\n" //Name (don't change)
+						+ "\nURBAN" //New State
+						+ "\n\n\n\n\n\n\n\n\n\n\n\n" //all other fields ignored
 						+ "5\n"; //Go Back
 
 			InputStream enterStream = new ByteArrayInputStream(input.getBytes());
